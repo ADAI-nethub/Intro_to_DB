f067d8f2da9d3e0ada3a5283ba5fbb2ca02885a8
ADAI-nethub <atomosdubaako@gmail.com>
2025-06-25 19:50:35 +0100

    Add script to list database tables

---
+++ task_3.sql
@@ -0,0 +1,8 @@
+-- task_3.sql
+
+-- This script lists all tables in a specified database.
+-- The database name should be passed as an argument to the mysql command,
+-- for example: mysql -u user -p database_name < task_3.sql
+
+SHOW TABLES;
+
