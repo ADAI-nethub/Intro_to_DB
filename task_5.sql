f5456c43e6d1382b56e732bab511e1b131e328e4
ADAI-nethub <atomosdubaako@gmail.com>
2025-06-26 11:16:53 +0100

    updates on task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -1,10 +1,4 @@
--- task_5.sql
+USE alx_book_store;
 
--- This script inserts a single row into the 'CUSTOMERS' table
--- in the database specified as an argument to the mysql command.
-
--- The database is assumed to be 'alx_book_store'.
-
-INSERT INTO CUSTOMERS (CUSTOMER_ID, CUSTOMER_NAME, EMAIL, ADDRESS) VALUES
+INSERT INTO customer (customer_id, customer_name, email, address) VALUES
 (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
-
